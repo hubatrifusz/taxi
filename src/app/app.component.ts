8a59f4059cd786777b0d00b479a03631122e90c6
HubaTrifusz <huba.trifusz@gmail.com>
2024-10-31 15:00:01 +0100

    footer component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { LogoComponent } from "./components/logo/logo.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LogoComponent],
+  imports: [RouterOutlet, NavbarComponent, LogoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
