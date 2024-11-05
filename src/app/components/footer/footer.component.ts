4e5ce4b2dd7547bd0b28cbf1a66253341a731db9
HubaTrifusz <huba.trifusz@gmail.com>
2024-11-05 22:40:12 +0100

    footer scroll animation removed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,30 +9,5 @@ import { CommonModule } from '@angular/common'; // Import CommonModule
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  scrolling: number = 100;
-  notVisible: boolean = true;
 
-  private clamp(value: number): number {
-    return Math.max(0, Math.min(101, value)); // Clamps the value between 0 and 100
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(event: WheelEvent) {
-    const scrolledHeight = window.scrollY;
-    const scrollableHeight =
-      document.documentElement.scrollHeight - window.innerHeight;
-
-    if (scrollableHeight === scrolledHeight) {
-      this.notVisible = false;
-
-      this.scrolling -= 1;
-      if (typeof event.deltaY === 'number' && this.scrolling < 100) {
-        event.preventDefault();
-        this.scrolling = this.clamp(this.scrolling - event.deltaY / 5);
-      }
-    } else {
-      this.scrolling = 100;
-      this.notVisible = true;
-    }
-  }
 }
