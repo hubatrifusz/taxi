5835b524eaa7f497b4c765707a07ff712aa6e418
HubaTrifusz <huba.trifusz@gmail.com>
2024-11-05 22:38:33 +0100

    reserve-card element added

---
+++ src/app/components/reserve-card/reserve-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserve-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './reserve-card.component.html',
+  styleUrl: './reserve-card.component.scss'
+})
+export class ReserveCardComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { ReserveCardComponent } from "../../components/reserve-card/reserve-card.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ReserveCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
