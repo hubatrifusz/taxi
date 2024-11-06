2113f58a54b972d3ff7e472451fdb817ea9103a2
HubaTrifusz <huba.trifusz@gmail.com>
2024-11-06 20:51:50 +0100

    reserve-card updates

--- src/app/components/reserve-card/reserve-card.component.ts
+++ src/app/components/reserve-card/reserve-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reserve-card',
   standalone: true,
   imports: [],
   templateUrl: './reserve-card.component.html',
-  styleUrl: './reserve-card.component.scss'
+  styleUrl: './reserve-card.component.scss',
 })
 export class ReserveCardComponent {
-
+  @Input() title: string = 'placeholder';
 }
