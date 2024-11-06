0e517284d7c804c80dba0e17a20e77fb075aa921
HubaTrifusz <huba.trifusz@gmail.com>
2024-11-06 20:53:34 +0100

    logo component removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { LogoComponent } from "./components/logo/logo.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CursorComponent } from "./components/cursor/cursor.component";
 import { HomeComponent } from "./pages/home/home.component";
@@ -10,7 +9,7 @@ import { HomeComponent } from "./pages/home/home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LogoComponent, FooterComponent, CursorComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CursorComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from "../logo/logo.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
