ada0cd435ad1ac60ca8869b57c6bdc308d72bc0c
HubaTrifusz <huba.trifusz@gmail.com>
2024-11-13 21:41:02 +0100

    navbar show on scroll up

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +10,42 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   showNavbar = false;
 
-  openNavbar() {
+  openNavigation() {
     const topSpan = document.querySelector('.navIcon')!.children[0];
     const bottomSpan = document.querySelector('.navIcon')!.children[1];
-    topSpan.classList.toggle('open')
+    const navbar = document.querySelector('.navbar')!;
+
+    navbar.classList.toggle('navbarOpen');
+    topSpan.classList.toggle('open');
     bottomSpan.classList.toggle('open');
 
     this.showNavbar = !this.showNavbar;
+
+    if (this.showNavbar) {
+      document.body.style.overflow = 'hidden'; // Disable scrolling
+    } else {
+      document.body.style.overflow = 'auto'; // Re-enable scrolling
+    }
   }
 
+  lastScrollTop = 0;
+
+  @HostListener('window:scroll')
+  toggleNavbar() {
+    const currentScrollTop =
+      window.scrollY || document.documentElement.scrollTop;
+    const element = document.querySelector('.navbar');
+
+    if (currentScrollTop > this.lastScrollTop) {
+      if (element) {
+        element.classList.add('hide');
+      }
+    } else {
+      if (element) {
+        element.classList.remove('hide');
+      }
+    }
+
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+  }
 }
