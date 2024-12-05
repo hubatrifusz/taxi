34c0110d9af4033ed64fb74c41bf75e2a7eabaa3
HubaTrifusz <huba.trifusz@gmail.com>
2024-12-05 22:39:50 +0100

    loading screen deleted, metadata added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CursorComponent } from "./components/cursor/cursor.component";
-import { LoadingScreenComponent } from "./components/loading-screen/loading-screen.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, CursorComponent, LoadingScreenComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CursorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
