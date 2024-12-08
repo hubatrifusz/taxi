fbe3627e22f5601c5528c7ba76ec2f0230098084
HubaTrifusz <huba.trifusz@gmail.com>
2024-12-08 23:17:15 +0100

    company form added

--- src/app/components/ride-details-form/ride-details-form.component.ts
+++ src/app/components/ride-details-form/ride-details-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CompanyFormComponent } from "../company-form/company-form.component";
 
 @Component({
   selector: 'app-ride-details-form',
   standalone: true,
-  imports: [],
+  imports: [CompanyFormComponent],
   templateUrl: './ride-details-form.component.html',
-  styleUrl: './ride-details-form.component.scss'
+  styleUrl: './ride-details-form.component.scss',
 })
 export class RideDetailsFormComponent {
+  option: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.option = params.get('option');
+    });
+  }
 }
